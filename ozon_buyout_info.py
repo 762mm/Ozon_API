b4e7e09d07529cd784c6e16745d6e531549eb96e
762mm <hans762@yandex.ru>
2026-02-09 22:35:17 +0300

    request data

--- ozon_buyout_info.py
+++ ozon_buyout_info.py
@@ -1,2 +1,50 @@
-print('test1')
-print('test2')
+import requests  # ignore
+from http import HTTPStatus
+import json
+import pandas as pd
+
+import locale
+import datetime
+
+
+client_id = '11777'
+api_key = '7cbbd14f-36d7-4611-80c5-69e359347dd0'
+
+url = "https://api-seller.ozon.ru/v1/analytics/data"
+
+
+date_start = '2026-01-15'
+date_end = '2026-01-15'
+metrics = ["revenue", "ordered_units"]
+dimensions = ["sku", "day"]
+filters = []
+sort = []
+limit = 10
+offset = 0
+
+
+payload = {
+    "date_from": date_start,  # Дата начала периода
+    "date_to": date_end,  # Дата окончания периода
+    "metrics": metrics,  # Метрики
+    "dimension": dimensions,  # Группировка
+    "filters": filters,  # Фильтр
+    "sort": sort,  # Сортировка
+    "limit": limit,  # Лимит в выдаче. Min: 1, max: 1000
+    "offset": offset  # Сколько чтрок пропустить в начале
+}
+
+headers = {
+    "Client-Id": client_id,
+    "Api-Key": api_key,
+    "Content-Type": "application/json"
+}
+
+
+response = requests.post(url, json=payload, headers=headers)
+
+data_result = response.json()["result"]["data"]
+
+print(data_result[1])
+
+
